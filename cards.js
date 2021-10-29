ccceab449451fe92bb00e40b0108bce233cdbb66
rom1dprt69 <82585301+rom1dprt69@users.noreply.github.com>
2021-10-29 21:59:59 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -9,3 +9,41 @@ for(var i=0;i<imgs.length;i++){
    e.target.src="medias/"+e.target.dataset.src;
   }
 }
+
+
+var imgs=document.getElementsByClassName('pull1');
+
+for(var i=0;i<imgs.length;i++){
+  imgs[i].onmouseenter=function(e){
+    e.target.src="medias/2pulldos.png";
+  }
+
+  imgs[i].onmouseout=function(e){
+   e.target.src="medias/"+e.target.dataset.src;
+  }
+}
+
+var imgs=document.getElementsByClassName('pull2');
+
+for(var i=0;i<imgs.length;i++){
+  imgs[i].onmouseenter=function(e){
+    e.target.src="medias/3pulldos.png";
+  }
+
+  imgs[i].onmouseout=function(e){
+   e.target.src="medias/"+e.target.dataset.src;
+  }
+}
+
+
+var imgs=document.getElementsByClassName('pull3');
+
+for(var i=0;i<imgs.length;i++){
+  imgs[i].onmouseenter=function(e){
+    e.target.src="medias/1pulldos.png";
+  }
+
+  imgs[i].onmouseout=function(e){
+   e.target.src="medias/"+e.target.dataset.src;
+  }
+}
