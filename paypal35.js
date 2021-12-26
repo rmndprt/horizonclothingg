7d3dcdb1f26f2efaae3c941f6f3117cb9cf7bbf0
rom1dprt69 <82585301+rom1dprt69@users.noreply.github.com>
2021-12-26 10:54:38 +0100

    Update paypal35.js

--- paypal35.js
+++ paypal35.js
@@ -6,7 +6,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units:[{
               amount: {
-                  value:'35.0'
+                  value:'40.0'
               }
             }]
         })
