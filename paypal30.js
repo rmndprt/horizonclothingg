acb2a63960f3741e73049aa82eb12428c71fab06
rom1dprt69 <82585301+rom1dprt69@users.noreply.github.com>
2021-12-26 10:54:15 +0100

    Update paypal30.js

--- paypal30.js
+++ paypal30.js
@@ -6,7 +6,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units:[{
               amount: {
-                  value:'30.0'
+                  value:'35.0'
               }
             }]
         })
